3783ac5ee4ad99fc81863f400c67356a30aa7b9a
jayis1 <jayis1@github.com>
2026-03-13 03:53:36 +0100

    Fix: Add cleanup directive to purge orphaned 'to_ai_phone' PJSIP arrays that are crashing the config compiler

--- setup-routing.sh
+++ setup-routing.sh
@@ -7,7 +7,7 @@ echo "Setting up AI Phone Routing in FreePBX (Manual Config Method)..."
 mysql -u root asterisk << 'SQL'
 DELETE FROM users WHERE extension = '9001';
 DELETE FROM devices WHERE id = '9001';
-DELETE FROM pjsip WHERE id = '9001' OR id = 'ai_phone_trunk' OR id = '2';
+DELETE FROM pjsip WHERE id = '9001' OR id = 'ai_phone_trunk' OR id = '2' OR id = 'to_ai_phone';
 DELETE FROM trunks WHERE trunkid = 2 OR channelid = 'ai_phone_trunk';
 DELETE FROM incoming WHERE destination LIKE '%9001%';
 FLUSH PRIVILEGES;
